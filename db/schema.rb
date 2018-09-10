8ad2c8a3ddf3b39b5e0db422748478ae954e50fb
mahert12 <mahert12.tmaher@gmail.com>
2018-09-10 19:13:05 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20180910182445) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.datetime "start"
