bcfc088e2977c5bd4c4ded7cf812295771651b55
mahert12 <mahert12.tmaher@gmail.com>
2018-09-11 13:52:29 -0400

    new countdown display

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ActiveRecord::Base
   scope :ends_next, ->() {
     where(end: DateTime.now..DateTime.now+24.hours)
   }
+  scope :upcoming, ->() {
+    where(start: DateTime.now..DateTime.now+1.year).order("start")
+  }
+  scope :next, ->(title) {
+    upcoming.where(title: title).first
+  }
 end
