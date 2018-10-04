399f7190f35b7c7bfe5e41d9b3ecf12dd19aeefb
mahert12 <mahert12.tmaher@gmail.com>
2018-10-04 14:22:19 -0400

    fixing database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ for i in 1..100
   @strikes = Event.create({title: 'Vanguard Daily', start: start_datetime, end: start_datetime+4.days, color: '#f36621', allDay: true, order: 1})
   @gambit = Event.create({title: 'Gambit Daily', start: start_datetime+1.day, end: start_datetime+5.days, color: '#0b7b4d', allDay: true, order: 2})
   @crucible = Event.create({title: 'Crucible Daily', start: start_datetime+2.day, end: start_datetime+6.days, color: '#c62b29', allDay: true, order: 3})
-  @heroic = Event.create({title: 'Daily Heroic Adventure', start: start_datetime+3.day, end: start_datetime+7.days, color: '#232b57', allDay: true, order: 4})
+  @heroic = Event.create({title: 'Daily Heroic Adventure', start: start_datetime+2.day, end: start_datetime+6.days, color: '#232b57', allDay: true, order: 4})
   start_datetime = start_datetime + 4.days
 end
 
